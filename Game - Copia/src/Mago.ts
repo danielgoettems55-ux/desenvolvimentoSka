ffb8433eadc9dcc80fbecd80665ed0c0e2a1a739
Lucas <danielgoettems55@gmail.com>
2026-05-12 20:04:30 -0300

    adicionando jogo novamente

---
+++ Game - Copia/src/Mago.ts
@@ -0,0 +1,58 @@
+import { Personagem } from "./Personagem";
+
+export class Mago extends Personagem {
+  constructor(nome: string, vida: number, dano: number, regen: number) {
+    super(
+      nome,
+      vida,
+      dano,
+      regen,
+      "./public/mago.PNG",
+    );
+  }
+  /* public atacar(persona: Personagem): void {
+        this.dano = this.verificarAtaque(this.gerarAtaque());
+       console.log("O " + this.nome + " atacou o "+persona.nome + " e causou "+this.dano+ " de dano\nVida ");
+        persona.sofrerAtaque(this.dano);
+        console.log(persona.nome +" = " + persona.getVida());
+    }*/
+
+  public atacar(persona: Personagem) {
+    let ataque_base = this.dano;
+    let dado = this.gerarAtaque();
+    let msg: string = "feitiço base";
+
+    switch (dado) {
+      case 1:
+        ataque_base += 20;
+        msg = "bola de fogo";
+        break;
+
+      case 2:
+        ataque_base += 30;
+        msg = "ralampago";
+        break;
+
+      case 3:
+        ataque_base += 50;
+        msg = "Decaimento";
+        break;
+
+      default:
+        break;
+    }
+    this.log(
+      "O " +
+        this.nome +
+        " atacou o " +
+        persona.nome +
+        " com o feitiço: " +
+        msg +
+        " e causou " +
+        ataque_base +
+        " de dano",
+    );
+    persona.sofrerAtaque(ataque_base);
+    this.log("Vida" + persona.nome + " = " + persona.getVida());
+  }
+}
